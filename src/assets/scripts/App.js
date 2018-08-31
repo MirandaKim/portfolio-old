86220e3878d8745ef6f5b5c192d9ed7a330ea1ab
Miranda Hoover <mkhoover89@gmail.com>
2018-08-31 13:25:24 -0700

    Adjusted section change waypoints. Gave sections a minimum height.

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -108,8 +108,8 @@ let section_selector = '.section';
 let section_activeLinkClassStr = `active ${mainNav_block}__link--active`;
 
 let sectionChange = new SectionChange(section_selector, section_activeLinkClassStr);
-// sectionChange.scrollUpOffset = "-5%";
-// sectionChange.scrollDownOffset="20%";
+sectionChange.scrollUpOffset = "-30%";
+sectionChange.scrollDownOffset="40%";
 sectionChange.setEvents();
 
 /**************************************/
